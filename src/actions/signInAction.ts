6a1a85dd529dabbbf71d279c4911f49d7683f7c1
Justin-K-Ellis <jlke45@gmail.com>
2026-04-07 22:01:55 +0900

    feat: create logic to sign user in

---
+++ src/actions/signInAction.ts
@@ -0,0 +1,26 @@
+import supabase from "../auth/supabase";
+import { redirect } from "react-router";
+
+export default async function signInAction({ request }: { request: Request }) {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+  if (!email || !password) {
+    return { error: "Email and password are required." };
+  }
+
+  try {
+    const { error: supabaseError } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (supabaseError) {
+      console.error(supabaseError);
+      return { error: supabaseError.message };
+    }
+    return redirect("/");
+  } catch (error) {
+    console.error(error);
+    return { error: "Something went wrong when signing in." };
+  }
+}
