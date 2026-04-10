8d5bdd577997daa5cdb93cd2b610aea5e76c84a1
Justin-K-Ellis <jlke45@gmail.com>
2026-04-10 23:13:01 +0900

    fix: move main out of root route

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,9 +5,7 @@ export default function RootLayout() {
   return (
     <>
       <Navbar />
-      <main className="mx-auto flex flex-col items-center w-11/12 md:w-1/2">
-        <Outlet />
-      </main>
+      <Outlet />
     </>
   );
 }
