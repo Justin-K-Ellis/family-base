8fa33c7a505ec44ce9f8984ee53f13a1fa83ccce
Justin-K-Ellis <jlke45@gmail.com>
2026-04-10 23:55:17 +0900

    fix: center signin/signup elements

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,9 @@
 import SignInSignUp from "../components/SignInSignUp";
 
 export default function SignInPage() {
-  return <SignInSignUp mode="signin" />;
+  return (
+    <main className="mx-auto flex flex-col items-center w-11/12 md:w-1/2">
+      <SignInSignUp mode="signin" />
+    </main>
+  );
 }
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,9 @@
 import SignInSignUp from "../components/SignInSignUp";
 
 export default function SignUpPage() {
-  return <SignInSignUp mode="signup" />;
+  return (
+    <main className="mx-auto flex flex-col items-center w-11/12 md:w-1/2">
+      <SignInSignUp mode="signup" />
+    </main>
+  );
 }
